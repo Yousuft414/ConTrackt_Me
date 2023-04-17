284d22b83ceabdd1cd97658d196d165bab5cc349
Zain_ <razvi@sheridancollege.ca>
2023-04-16 23:59:08 -0400

    Created navigation between the MainActivity.kt and Registration.kt, as well as some functionality for the buttons.

--- app/src/main/java/com/example/contrackt_me/view/MainActivity.kt
+++ app/src/main/java/com/example/contrackt_me/view/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.contrackt_me.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.contrackt_me.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text1: EditText = findViewById(R.id.txtEmail)
+        val text2: EditText = findViewById(R.id.txtPwd)
+
+        val textView: TextView= findViewById(R.id.lnkRegister)
+        textView.setOnClickListener{
+            //Toast.makeText(this, "Test click", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Registration::class.java)
+            startActivity(intent)
+        }
+
+        val button: Button =findViewById(R.id.btnLogin)
+        button.setOnClickListener{
+            Toast.makeText(this, "User authenticated", Toast.LENGTH_SHORT).show()
+            text1.getText().clear()
+            text2.getText().clear()
+        }
     }
 }
--- app/src/main/java/com/example/contrackt_me/view/Registration.kt
+++ app/src/main/java/com/example/contrackt_me/view/Registration.kt
@@ -2,11 +2,28 @@ package com.example.contrackt_me.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.contrackt_me.R
 
 class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val text1: EditText = findViewById(R.id.txtEmail)
+        val text2: EditText = findViewById(R.id.Name)
+        val text3: EditText = findViewById(R.id.txtphoneNum)
+        val text4: EditText = findViewById(R.id.txtPwd)
+
+        val button: Button =findViewById(R.id.btnLogin)
+        button.setOnClickListener{
+            Toast.makeText(this, "New account created", Toast.LENGTH_SHORT).show()
+            text1.getText().clear()
+            text2.getText().clear()
+            text3.getText().clear()
+            text4.getText().clear()
+        }
     }
 }
