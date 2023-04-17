da223947a7e463ea39acaf469f92cb8ab28573b8
RPerocho <perocho@sheridancollege.ca>
2023-04-17 11:55:47 -0400

    added home screren. added job request home screen.

---
+++ app/src/main/java/com/example/contrackt_me/view/JobRequestHomeActivity.kt
@@ -0,0 +1,28 @@
+package com.example.contrackt_me.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.contrackt_me.R
+
+class JobRequestHomeActivity : AppCompatActivity() {
+    private lateinit var viewJobRequestsButton: Button
+    private lateinit var createJobRequestButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.job_request_home_page)
+
+        viewJobRequestsButton = findViewById(R.id.view_job_requests_button)
+        createJobRequestButton = findViewById(R.id.create_job_request_button)
+
+        viewJobRequestsButton.setOnClickListener {
+            // todo: create job viewing
+        }
+        createJobRequestButton.setOnClickListener {
+            val intent = Intent(this, JobRequestActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
