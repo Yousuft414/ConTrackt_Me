47df776f023fda31c4b93be89bbcaae43a5b47d8
RPerocho <perocho@sheridancollege.ca>
2023-04-18 12:28:21 -0400

    modified view job requests list items.

--- app/src/main/java/com/example/contrackt_me/util/JobRequestAdapter.kt
+++ app/src/main/java/com/example/contrackt_me/util/JobRequestAdapter.kt
@@ -22,9 +22,15 @@ class JobRequestsAdapter(context: Context, jobRequests: List<JobRequest>) :
 
         val titleTextView = listItemView!!.findViewById<TextView>(R.id.job_title_text_view)
         val descriptionTextView = listItemView.findViewById<TextView>(R.id.job_description_text_view)
+        val certsTextView = listItemView.findViewById<TextView>(R.id.job_required_certs_text_view)
+        val minHourlyTextView = listItemView.findViewById<TextView>(R.id.job_min_hourly_text_view)
+        val maxHourlyTextView = listItemView.findViewById<TextView>(R.id.job_max_hourly_text_view)
 
         titleTextView.text = jobRequest!!.title
-        descriptionTextView.text = jobRequest.description
+        descriptionTextView.text = "description: " + jobRequest.description
+        certsTextView.text = "required certification: " + jobRequest.requiredCerts
+        minHourlyTextView.text = "$" + jobRequest.minHourly.toString() + "/hr"
+        maxHourlyTextView.text = "$" + jobRequest.maxHourly.toString() + "/hr"
 
         return listItemView
     }
