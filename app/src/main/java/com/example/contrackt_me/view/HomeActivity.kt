5f468808c62d1bbcf7295b1dbdd519eb7bc7ade9
Yousuft414 <yousuft@sheridancollege.ca>
2023-04-18 13:20:28 -0400

    Updated manifest to use internet and location

--- app/src/main/java/com/example/contrackt_me/view/HomeActivity.kt
+++ app/src/main/java/com/example/contrackt_me/view/HomeActivity.kt
@@ -14,10 +14,17 @@ class HomeActivity : AppCompatActivity() {
 
         // Retrieve the Job Request button element
         val jobRequestButton: Button = findViewById(R.id.job_request_button)
+        val filterCompareButton: Button = findViewById(R.id.filter_compare_button)
 
         // Set up an OnClickListener for the Job Request button
         jobRequestButton.setOnClickListener {
             val intent = Intent(this, JobRequestHomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Set up an OnClickListener for the Filter and Compare button
+        filterCompareButton.setOnClickListener {
+            val intent = Intent(this, contractorList::class.java)
             startActivity(intent)
         }
     }
